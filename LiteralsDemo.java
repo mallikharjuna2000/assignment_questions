d9175740a2d838aafc607a358737f22fd1c8864b
maniganesh-codegnan <maniganesh@codegnan.com>
2025-10-15 11:59:22 +0530

    day-1

---
+++ LiteralsDemo.java
@@ -0,0 +1,12 @@
+public class LiteralsDemo{
+	public static void main(String[] args){
+		int num1=10;
+		int num2=010;
+		int num3=0x10;
+		int num4=0b10;
+		System.out.println(num1);
+		System.out.println(num2);
+		System.out.println(num3);
+		System.out.println(num4);
+	}
+}
